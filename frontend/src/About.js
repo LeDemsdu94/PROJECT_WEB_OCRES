682f7c5d547414bf60709dbc40c61d9c597c18e0
LeDemsdu94 <damien.mehaut@numericable.fr>
2021-11-23 14:22:48 +0100

    Routage

---
+++ frontend/src/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function About(){
+    return(
+        <div>
+            <h2>Hello about us</h2>
+            <p>We are doing a projet d'info.</p>
+        </div>
+    )
+}
+export default About;
---
+++ frontend/src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Home(){
+    return(
+        <div>
+            <h2>Hello wrorld</h2>
+            <p>Stare at the celing now we here.</p>
+        </div>
+    )
+}
+export default Home;
